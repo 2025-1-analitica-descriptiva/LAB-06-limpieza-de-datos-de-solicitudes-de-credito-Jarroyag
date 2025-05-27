8a986210fb87283b54ff371916d97a197f874fc5
Jorge Arroyave <joarroyaveg@unal.edu.co>
2025-05-26 21:47:00 -0500

    Lab_06

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,15 +1,54 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en la pregunta.
-"""
+import pandas as pd
+import os 
 
+def clean_text_cols(df: pd.DataFrame, cols: list):
+    df = df.copy()
+    for col in cols:
+       
+        df[col] = df[col].str.lower()
+        df[col] = df[col].str.replace('_', ' ')
+        df[col] = df[col].str.replace('-', ' ')
+       
+    return df
+
+def save_df(df: pd.DataFrame):
+    if not os.path.exists('files/output'):
+        os.mkdir('files/output')
+    df.to_csv('files/output/solicitudes_de_credito.csv',index=True,sep=';')
 
 def pregunta_01():
-    """
-    Realice la limpieza del archivo "files/input/solicitudes_de_credito.csv".
-    El archivo tiene problemas como registros duplicados y datos faltantes.
-    Tenga en cuenta todas las verificaciones discutidas en clase para
-    realizar la limpieza de los datos.
+    
+    df = pd.read_csv('files/input/solicitudes_de_credito.csv', sep=';', index_col=0)
+    df.drop_duplicates(keep='first', inplace=True)
+    df.dropna(inplace=True)
+    df = clean_text_cols(df, ['sexo','tipo_de_emprendimiento','idea_negocio','barrio','línea_credito'])
+    
+    df.monto_del_credito = df.monto_del_credito.map(lambda x: float(x.replace('$','').replace(',','').replace(".00", "").strip() if isinstance(x,str) else x))
+    
+    
+    df["year"] = df["fecha_de_beneficio"].map(
+        lambda x: (
+            int(x.split("/")[0])
+            if len(x.split("/")[0]) > 2
+            else int(x.split("/")[-1])
+        )
+    )
+    df["month"] = df["fecha_de_beneficio"].map(lambda x: int(x.split("/")[1]))
+    df["day"] = df["fecha_de_beneficio"].map(
+        lambda x: (
+            int(x.split("/")[-1])
+            if len(x.split("/")[0]) > 2
+            else int(x.split("/")[0])
+        )
+    )
+    df["fecha_de_beneficio"] = pd.to_datetime(df[["year", "month", "day"]])
 
-    El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
+    df = df.drop(columns=["year", "month", "day"])
 
-    """
+    df.drop_duplicates(keep='first', inplace=True)
+    
+    save_df(df)
+    return df
+
+
+pregunta_01()
